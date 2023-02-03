ac6ff86fd4a511423ab2a92d7fcced60e6449564
HunterAkeriano <gamerstaject@gmail.com>
2023-02-03 11:16:55 +0200

    add file city

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const burger = document.querySelector('.humb');
+const headerNav = document.querySelector('.header__nav');
+const headerHover = document.querySelector('.header');
+
+function openMenu(){
+   let nav = burger.classList.toggle('active');
+    headerNav.classList.toggle('active');
+    if(nav){
+        document.body.style.overflow = 'hidden';
+        headerHover.style.opacity = 1;
+    }if(!nav){
+        document.body.style.overflow = 'auto';
+        headerHover.style.opacity = 0.7;
+    }
+}
+const screenWidth = window.screen.width
+
+function openContext(){
+    burger.classList.toggle('active');
+
+}
+burger.addEventListener('click', openMenu);
+
+
+
+
